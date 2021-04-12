d5672207e7cf2e90866a7e920307be0533b752b6
Blake Wyatt <blakewyatt7@gmail.com>
2021-04-12 12:15:09 -0500

    first commit

---
+++ BMI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring Variables
+            double weight;
+            double height;
+
+            //Getting Input
+            Console.Write("Please enter your weight in pounds: ");
+            weight = double.Parse(Console.ReadLine());
+            Console.Write("Please enter your height in inches: ");
+            height = double.Parse(Console.ReadLine());
+            BMICalculation(weight, height);
+
+        }//End of Main
+
+        //Creating Functions
+        static void BMICalculation(double weight, double height)
+        {
+            double bmi = (weight * 703) / (Math.Pow(height, 2));
+            Console.WriteLine("Your BMI is: {0:F1}", bmi);
+        }
+
+
+    }
+}
